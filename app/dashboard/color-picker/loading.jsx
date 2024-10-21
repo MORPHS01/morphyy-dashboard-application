16b9d1b5b733cc0f0e47fa43ad017d3a1c4f51f3
MORPHS01 <ayonimofe2202@gmail.com>
2024-10-21 09:44:47 +0100

    loader fix

---
+++ app/dashboard/color-picker/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className="flex h-[80vh] w-[100%] justify-center items-center">
+        <div className="loader"/>
+    </div>
+  )
+}
