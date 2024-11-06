152951fbd93b53634f440a6357e486d5e75db274
MORPHS01 <ayonimofe2202@gmail.com>
2024-11-06 10:51:48 +0100

    UI fixes

--- app/dashboard/pie/page.jsx
+++ app/dashboard/pie/page.jsx
@@ -23,7 +23,7 @@ function pie() {
         <h3 className="text-2xl tracking-wide text-slate-900">Project Cost Breakdown</h3>
       </div>
 
-      <div className="flex justify-center items-center mb-7 w-[100%] ml-5 mx-auto max-sm:w-[121%]">
+      <div className="flex justify-center items-center mb-7 w-[110%] ml-5 mx-auto max-sm:w-[121%]">
         <PieChart
           series={[
             {
